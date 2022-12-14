596bbcd95369cede3f98423ca7860e57559200e8
Yehan Yeshminda <yeshanyesh12@gmail.com>
2022-12-14 17:51:23 +0530

    added the error services

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,7 +1,5 @@
-using API.Data;
 using API.Extensions;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
+using API.Middlewares;
 
 namespace API
 {
@@ -25,9 +23,11 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseMiddleware<ExceptionMiddleware>();
+            
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
@@ -36,6 +36,9 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
