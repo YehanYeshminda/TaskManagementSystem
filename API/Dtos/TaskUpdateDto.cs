d3c0ebec9e8a49aa051a9395b2e370c071e66af4
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2022-12-29 05:48:21 +0530

    added the updated version of updated for the tasks

--- API/Dtos/TaskUpdateDto.cs
+++ API/Dtos/TaskUpdateDto.cs
@@ -2,13 +2,8 @@ namespace API.Dtos
 {
     public class TaskUpdateDto
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string CustomerDetails { get; set; }
         public int Qty { get; set; }
         public string Status { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
     }
 }
--- API/Entities/Inventory.cs
+++ API/Entities/Inventory.cs
@@ -8,8 +8,8 @@ namespace API.Entities
         public decimal UnitCost { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime? UpdatedAt { get; set; }
-        public DateTime? DeletedAt { get; set; }  
+        public DateTime? DeletedAt { get; set; }
         public Materials Materials { get; set; }
-        public Grn Grn { get; set; }        
+        public Grn Grn { get; set; }
     }
 }
